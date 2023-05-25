0e96818c4ed808c66fde6a1e1362f76c0f04543c
mihaja razafindrazaka <mihajarazafindrazaka@gmail.com>
2023-05-25 10:56:34 +0200

    exo 2 restaurants yelp

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,20 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+puts "Cleaning database..."
+Restaurant.destroy_all
+
+puts "Creating restaurants..."
+
+  Sushishop = {name: "Sushishop", address: "7 Boundary St, London E2 7JE", phone_number: "0128383893", category:"japanese"}
+  Pizzapino =  {name: "Pizzapino", address: "56A Shoreditch High St, London E1 6PQ", phone_number: "0127728493", category:"italian"}
+  Pekinexpress =  {name: "Pekinexpress", address: "34 rue gaudelet", phone_number: "0134728487", category:"chinese"}
+  Brux =  {name: "Brux", address: "54 av de choisy", phone_number: "0199928493", category:"belgian" }
+  Louis14 =  {name: "Louis14", address: "1 rue de la paix", phone_number: "0134728493", category:"french"}
+
+[Sushishop, Pizzapino, Pekinexpress, Brux, Louis14 ].each do |attributes|
+  restaurant = Restaurant.create!(attributes)
+  puts "Created #{restaurant.name}"
+end
+puts "Finished!"
